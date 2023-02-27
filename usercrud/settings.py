a69b2f2cfb711d22ebe9f5a24ad2bd187ab29710
vynious <shawnthiah@gmail.com>
2023-02-28 01:22:31 +0800

    update

--- usercrud/settings.py
+++ usercrud/settings.py
@@ -19,6 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-zzwjz8m^7(sstl0(5rn4yh-=1l+grk8m-rnidcz8%pr4fu-fmy'
+
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
